4b11fddc620da699ac8d3c69d2a039ca5e389364
sciencegum5 <32943597+sciencegum5@users.noreply.github.com>
2018-08-16 13:43:22 -1000

    Add files via upload

---
+++ userushdemo.js
@@ -0,0 +1,9 @@
+var first = [];
+var last = [];
+var questionFirst = prompt('enter a first name please');
+first.push(questionFirst);
+//window.alert(first[0]);
+var questionLast = prompt('enter a last name please');
+last.push(questionLast);
+//window.alert(last[0]);
+window.alert(last + ',' + first + ' ' +last.length + ',' + first.length);
